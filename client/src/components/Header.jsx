9b7a5f95fee11221bc27b0a1aa33c85b93803dc0
Cyrus Chan <sairuscsc@gmail.com>
2023-10-15 13:11:02 -0400

    init server; modify Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,11 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { routes } from '../constants';
-import { FaBars, FaMagnifyingGlass, FaXmark } from 'react-icons/fa6';
+import {
+  FaMagnifyingGlass,
+  FaRegCircleUser,
+  FaRegLightbulb,
+} from 'react-icons/fa6';
 import logo from '../assets/yardgrown-wenge.png';
 
 export default function Header() {
@@ -12,7 +16,7 @@ export default function Header() {
 
   return (
     <header className='bg-leaves text-wenge text-base font-medium'>
-      <div className='flex items-center justify-between max-w-7xl mx-auto px-8 py-4 max-md:flex-col max-md:items-start'>
+      <div className='flex items-center justify-between max-w-7xl mx-auto px-8 py-4'>
         <Link
           to={routes.home}
           className='flex gap-2 items-center justify-start text-xl'
@@ -22,53 +26,25 @@ export default function Header() {
             Yard<span className='text-grullo'>Grown</span>
           </h1>
         </Link>
-
-        <nav className={`${dropdownShown ? 'max-md:w-full' : ''}`}>
-          <Link className='absolute top-6 right-10 text-2xl invisible max-md:visible nav-hover'>
-            {dropdownShown ? (
-              <FaXmark onClick={toggleDropdown} />
-            ) : (
-              <FaBars onClick={toggleDropdown} />
-            )}
-          </Link>
-          <ul
-            className={`flex items-center justify-center max-md:flex-col max-md:items-start max-md:w-full gap-4 ${
-              dropdownShown ? 'max-md:flex max-md:mt-4' : 'max-md:hidden'
-            }`}
-          >
-            <li>
-              <form className='flex items-center justify-center border-2 border-wenge rounded-lg px-2 py-1 text-base'>
-                <input
-                  type='text'
-                  placeholder='Search'
-                  className='bg-transparent w-full focus:outline-none'
-                />
-                <FaMagnifyingGlass />
-              </form>
-            </li>
+        <form className='hidden sm:flex items-center justify-center border-2 border-wenge rounded-lg px-2 py-1 text-base'>
+          <input
+            type='text'
+            placeholder='Search'
+            className='bg-transparent w-64 md:w-96 focus:outline-none'
+          />
+          <FaMagnifyingGlass />
+        </form>
+        <nav>
+          <ul className='flex items-center justify-center gap-8 text-xl'>
+            <li></li>
             <li className='nav-hover'>
               <Link to={routes.about} onClick={hideDropdown}>
-                About
+                <FaRegLightbulb />
               </Link>
             </li>
             <li className='nav-hover'>
               <Link to={routes.profile} onClick={hideDropdown}>
-                Profile
-              </Link>
-            </li>
-            <li className='nav-hover'>
-              <Link to={routes.signUp} onClick={hideDropdown}>
-                Sign Up
-              </Link>
-            </li>
-            <li className='nav-hover'>
-              <Link to={routes.signIn} onClick={hideDropdown}>
-                Sign In
-              </Link>
-            </li>
-            <li className='nav-hover'>
-              <Link to={routes.signOut} onClick={hideDropdown}>
-                Sign Out
+                <FaRegCircleUser />
               </Link>
             </li>
           </ul>
