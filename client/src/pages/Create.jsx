b3ba9b66b387f8ed4615478fc9d087b743c44898
Cyrus Chan <sairuscsc@gmail.com>
2023-10-22 03:27:21 -0400

    complete upload listing images functionality

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -1,6 +1,72 @@
+import { useState } from 'react';
 import { categories } from '../constants';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase';
 
 export default function Create() {
+  const [files, setFiles] = useState([]);
+  const [uploading, setUploading] = useState(false);
+  const [uploadError, setUploadError] = useState(false);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  });
+
+  const handleFilesUpload = () => {
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      setUploading(true);
+      setUploadError(false);
+      const promises = [];
+      for (let i = 0; i < files.length; i++) promises.push(storeFile(files[i]));
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setUploading(false);
+          setUploadError(false);
+        })
+        .catch((err) => {
+          setUploading(false);
+          setUploadError('Upload failed, maximum 2 MB per image');
+        });
+    } else {
+      setUploading(false);
+      setUploadError('Maximum 6 images only');
+    }
+  };
+
+  const storeFile = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + '-' + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {},
+        (error) => reject(error),
+        () =>
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+            resolve(downloadURL)
+          )
+      );
+    });
+  };
+
+  const handleFileDelete = (index) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+    });
+  };
+
   return (
     <main className='max-w-7xl px-4 mx-auto'>
       <h1 className='title'>Bring a product to the market</h1>
@@ -90,16 +156,40 @@ export default function Create() {
               accept='image/*'
               required
               multiple
-              id='imageUrls'
+              onChange={(e) => setFiles(e.target.files)}
+              id='images'
               className='create-input'
             />
             <button
               type='button'
+              disabled={uploading}
+              onClick={handleFilesUpload}
               className='rounded-lg p-3 font-medium uppercase border border-leaves text-leaves hover:bg-leaves hover:text-snow duration-300'
             >
-              Upload
+              {uploading ? 'Uploading' : 'Upload'}
             </button>
           </div>
+          {uploadError && <p className='text-pink px-3'>{uploadError}</p>}
+          {formData.imageUrls.length > 0 &&
+            formData.imageUrls.map((url, index) => (
+              <div
+                key={index}
+                className='flex items-center justify-between border border-leaves rounded-lg p-3 '
+              >
+                <img
+                  src={url}
+                  alt='Product Image'
+                  className='h-24 w-24 object-contain rounded-lg'
+                />
+                <button
+                  type='button'
+                  onClick={() => handleFileDelete(index)}
+                  className='rounded-lg p-3 font-medium uppercase border border-pink text-pink hover:bg-pink hover:text-snow duration-300'
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
           <button className='auth-button'>Create Listing</button>
         </div>
       </form>
