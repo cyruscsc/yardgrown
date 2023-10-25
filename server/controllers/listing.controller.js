074a86ef916293680ec63b2d9799947e8e98a50a
Cyrus Chan <sairuscsc@gmail.com>
2023-10-24 20:21:24 -0400

    create update listing api route

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -10,6 +10,23 @@ export const createListing = async (req, res, next) => {
   }
 };
 
+export const updateListing = async (req, res, next) => {
+  const currListing = await Listing.findById(req.params.id);
+  if (!currListing) return next(errorHandler(404, 'Listing not found'));
+  if (req.user.id !== currListing.userRef)
+    return next(errorHandler(401, 'User ID Mismatch'));
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const deleteListing = async (req, res, next) => {
   const currListing = await Listing.findById(req.params.id);
   if (!currListing) return next(errorHandler(404, 'Listing not found'));
